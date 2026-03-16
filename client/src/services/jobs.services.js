dc09f1771b3402be43dfe30c1d289c44d317f3e1
Meshil Maring <dsmeshilmaring13@gmail.com>
2026-03-17 00:05:23 +0530

    update: completed job for frontend

--- client/src/services/jobs.services.js
+++ client/src/services/jobs.services.js
@@ -15,7 +15,7 @@ export const postJobs = async (data) => {
 
 // GET : fetch jobs by user id
 export const getJobsByUserID = async (user_id) => {
-  const res = await fetch(`${URL}/api/jobs/:$${user_id}`, {
+  const res = await fetch(`${URL}/api/jobs/${user_id}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -25,4 +25,39 @@ export const getJobsByUserID = async (user_id) => {
   const data = res.json();
 
   return data;
+};
+// UPDATE (PUT) : update job by job id
+export const updateByJobId = async (job_id, data) => {
+  try {
+    const res = await fetch(`${URL}/api/jobs/update/${job_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+// DELETE : delete job by job id
+export const deleteByJobId = async (job_id) => {
+  try {
+    const res = await fetch(`${URL}/api/jobs/${job_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return await res.json();
+  } catch (err) {
+    return err;
+  }
 };
